1abc53b67f3d12610262ecfc1c2fbd379fe23299
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-11-25 01:53:44 +0100

    Upload New File

---
+++ Bank_app/Client.java
@@ -0,0 +1,44 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client extends PersonalData {
+	private String client_id, employees_id;
+	
+	public String getClient_id() {
+		return client_id;
+	}
+
+	public String getEmployees_id() {
+		return employees_id;
+	}
+	public Employee getEmployee() throws SQLException, WrongId {
+		return new Employee(this.employees_id);
+	}
+	public List<BankAccount> getBankAccountes() throws SQLException {
+		Statement statement = Main.conn.createStatement();
+		ResultSet results = statement.executeQuery("SELECT account_id, balance, account_no, start_date, end_date, interest_rate, accum_period, installment_size, account_types_f_id, clients_f_id from bank_accounts where clients_f_id = "+this.client_id);
+		List<BankAccount> accounts = new ArrayList<BankAccount>();
+		while (results.next()) {
+			accounts.add(new BankAccount(results));
+		}
+		return accounts;
+	}
+	public Client(String data_id) throws SQLException, WrongId {
+		super(data_id);
+		Statement statement = Main.conn.createStatement();
+		ResultSet results = statement.executeQuery("SELECT client_id, EMPLOYEES_F_ID from clients where PERSONAL_DATA_F_ID = "+data_id);
+		if (results.next()) { // if not empty
+			this.client_id = results.getString(1);
+			this.employees_id = results.getString(2);
+		} else
+			throw new WrongId(data_id);
+	}
+
+	@Override
+	public String toString() {
+		return "Client [client_id=" + client_id + ", employees_id=" + employees_id + "]";
+	}
+}
