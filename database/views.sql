5eed15d9006b5bdbcecb762913ee6050ff9bf765
Arkadiusz Kojtek <01158743@pw.edu.pl>
2021-12-08 13:29:53 +0100

    Dodano 2 nowe widoki i 3 nowe tabele, zmieniono sporo logiki bazy danych. Czy bedzie działać okaże się w przyszłości.

---
+++ database/views.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW v_bank_accounts AS
+SELECT b.bank_account_id, s.balance, s.start_date, s.accum_period, s.interest, s.client_id, t.name
+FROM bank_accounts b INNER JOIN SERVICES_INFO s USING(service_info_id) INNER JOIN Account_types t ON t.account_type_id = b.account_types_type_id;
+
+CREATE OR REPLACE VIEW v_loans AS
+SELECT l.loan_id, l.initial_value, s.balance, l.installment, s.start_date, l.end_date, s.accum_period, s.interest, s.client_id
+FROM loans l INNER JOIN SERVICES_INFO s USING(service_info_id);
