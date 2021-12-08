b186203eb2516a2274a827619fa72ce843623662
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-12-08 21:01:21 +0100

    Added new view for address class

--- database/views.sql
+++ database/views.sql
@@ -5,3 +5,8 @@ FROM bank_accounts b INNER JOIN SERVICES_INFO s USING(service_info_id) INNER JOI
 CREATE OR REPLACE VIEW v_loans AS
 SELECT l.loan_id, l.initial_value, s.balance, l.installment, s.start_date, l.end_date, s.accum_period, s.interest, s.client_id
 FROM loans l INNER JOIN SERVICES_INFO s USING(service_info_id);
+
+--address view
+CREATE OR REPLACE VIEW v_addresses AS
+SELECT a.address_id, a.street, a.apartment_no, c.city_name
+FROM addresses a INNER JOIN cities c ON a.CITIES_CITY_ID = c.CITY_ID;
