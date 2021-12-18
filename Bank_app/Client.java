5b9b3a695ace64ed85cb5f6d1b0bced0ab891633
Aleksandra Sypula <aleksandra.sypula@gmail.com>
2021-12-18 18:32:00 +0100

    Loan class added

--- Bank_app/Client.java
+++ Bank_app/Client.java
@@ -19,7 +19,8 @@ public class Client extends PersonalData {
 	}
 	public List<BankAccount> getBankAccounts() throws SQLException {
 		Statement statement = Main.conn.createStatement();
-		ResultSet results = statement.executeQuery("SELECT bank_account_id, balance, account_no, start_date, end_date, interest_rate, accum_period, installment_size, account_types_type_id, clients_client_id from bank_accounts where clients_client_id = "+this.client_id);
+		String query = "SELECT bank_account_id, balance, account_no, start_date, interest, accum_period, account_types_type_id, client_id, name FROM v_bank_accounts where client_id = "+this.client_id;
+		ResultSet results = statement.executeQuery(query);
 		List<BankAccount> accounts = new ArrayList<BankAccount>();
 		while (results.next()) {
 			accounts.add(new BankAccount(results));
@@ -29,7 +30,8 @@ public class Client extends PersonalData {
 	public Client(String data_id) throws SQLException, WrongId {
 		super(data_id);
 		Statement statement = Main.conn.createStatement();
-		ResultSet results = statement.executeQuery("SELECT client_id, employees_employee_id from clients where PERSONAL_DATA_DATA_ID = "+data_id);
+		String query = "SELECT client_id, employees_employee_id from clients where PERSONAL_DATA_DATA_ID = " + data_id;
+		ResultSet results = statement.executeQuery(query);
 		if (results.next()) { // if not empty
 			this.client_id = results.getString(1);
 			this.employees_id = results.getString(2);
@@ -40,13 +42,15 @@ public class Client extends PersonalData {
 			,String addresses_address_id) {
 		super(name,surname,pesel,phone_no,addresses_address_id);
 		this.client_id = null;
-		this.employees_id = "1"; //I'm a sad client I don't know how to get my emplotee ;(
+		this.employees_id = "1"; //I'm a sad client I don't know how to get my employee ;(
 	}
 	public void insert(String unhashed_password) throws SQLException { //for id = null
 		super.insert(unhashed_password);
 		Statement statement = Main.conn.createStatement();
-		statement.executeQuery("INSERT INTO CLIENTS(PERSONAL_DATA_DATA_ID,EMPLOYEES_EMPLOYEE_ID) VALUES("+data_id+","+employees_id+")");
-		ResultSet results = statement.executeQuery("Select CLIENT_ID from CLIENTS where PERSONAL_DATA_DATA_ID =" + data_id);
+		String query1 = "INSERT INTO clients(personal_data_data_id, employees_employee_id) VALUES(" + data_id + ","+ employees_id + ")";
+		statement.executeQuery(query1);
+		String query2 = "SELECT client_id FROM clients WHERE personal_data_data_id =" + data_id;
+		ResultSet results = statement.executeQuery(query2);
 		results.next();
 		this.client_id = results.getString(1);
 	}
