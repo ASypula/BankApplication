6b20f4b900013f94838b3ddb4b2385baccd05fcb
Arkadiusz Kojtek <01158743@pw.edu.pl>
2022-01-03 01:56:21 +0100

    poprawiono błąd kompilacji w funkcji last_transaction_amount - odwolywanie sie do slowa kluczowego date zamiast kolumny "Date"
    zmieniono nazwy funkcji na zaczynajace sie od f, dla utrzymania konwencji czy cos 
    
    usunieto procedury - korzystamy z identity do generowania sekwencji wiec te dodane byly zbedne
    
    dodano procedure do zmiany wartosci kursu walut
    
    dodano funkcje przeliczającą pieniadze na inny kurs
    
    dodano procedure wykonujacą te zmiany na koncie
    
    zmieniono funkcje year installment by obliczala wysokosc rat na dany rok z uwzglednieniem daty wywolania oraz ile zostalo do splaty

--- database/functions_procedures_seq.sql
+++ database/functions_procedures_seq.sql
@@ -1,58 +1,61 @@
-CREATE SEQUENCE next_client start with 100 maxvalue 1000 minvalue 0;
-CREATE SEQUENCE next_person start with 100 maxvalue 1000 minvalue 0;
-
--- adds new client to personal_data and client tables
-CREATE PROCEDURE add_client 
-(p_name VARCHAR2, p_surname VARCHAR2, p_pesel VARCHAR2, p_phone_no VARCHAR2, p_pwd VARCHAR2, p_address_id NUMBER)
-AS
-    new_id      clients.client_id%TYPE;
-    pers_id     clients.personal_data_data_id%TYPE;
-    emp_id      clients.employees_employee_id%TYPE;
-BEGIN
-    SELECT next_client.NEXTVAL INTO new_id FROM dual;
-    SELECT next_person.NEXTVAL INTO pers_id FROM dual;
-    SELECT employee_id INTO emp_id FROM employees FETCH FIRST 1 ROWS ONLY;
-    INSERT INTO personal_data VALUES (pers_id, p_name, p_surname, p_pesel, p_phone_no, p_pwd, p_address_id);
-    INSERT INTO clients VALUES (new_id, pers_id, emp_id);
-END;
-
--- removes a client from client table using client_id
-CREATE PROCEDURE remove_client_with_client_id (p_client_id VARCHAR2)
-AS
-BEGIN
-    DELETE FROM clients WHERE client_id=p_client_id;
-END;
-
--- removes client from client table using personal_id
-CREATE PROCEDURE remove_client_with_pers_id (p_pers_id VARCHAR2)
-AS
-    s_client_id     clients.client_id%TYPE;
-BEGIN
-    SELECT client_id INTO s_client_id FROM personal_data WHERE personal_data_id=p_pers_id;
-    remove_client_client_id(s_client_id);
-END;
-
--- returns total amount of installments to be paid in one year
-CREATE OR REPLACE FUNCTION year_installments (p_loan_id VARCHAR2)
+create or replace FUNCTION       "F_CONVERT_ACC_CURRENCY" (p_amount NUMBER, p_exchange_rate NUMBER, p_new_exchange_rate NUMBER)
 RETURN NUMBER
 AS
-    single_install   loans.installment%TYPE;
+    v_new_amount NUMBER;
 BEGIN
-    SELECT installment INTO single_install FROM loans WHERE loan_id=p_loan_id;
-    RETURN 12*single_install;
+    v_new_amount := p_amount * p_exchange_rate;
+    v_new_amount := v_new_amount / p_new_exchange_rate;
+
+    RETURN v_new_amount;
 END;
 
--- returns amount of most recent transaction made by client with given client_id
-CREATE OR REPLACE FUNCTION last_transaction_amount (p_client_id VARCHAR2)
+create or replace FUNCTION       "F_LAST_TRANSACT_VAL" (p_client_id VARCHAR2)
 RETURN NUMBER
 AS
     last_paym_am   transaction_history.amount%TYPE;
 BEGIN
-    SELECT amount INTO last_paym_am FROM transaction_history 
-    JOIN bank_accounts USING(bank_account_id) 
+    SELECT amount INTO last_paym_am 
+    FROM transaction_history JOIN bank_accounts USING(bank_account_id) 
     JOIN services_info USING(service_info_id)
     WHERE client_id=p_client_id
-    ORDER BY (date) desc FETCH FIRST 1 ROWS ONLY;
-    
+    ORDER BY ("Date") desc FETCH FIRST 1 ROWS ONLY;
+
     RETURN last_paym_am;
+END;
+
+create or replace FUNCTION       "F_NEXT_INSTALLMENT" (p_loan_id NUMBER)
+RETURN DATE
+AS
+    v_date   v_loans.START_DATE%TYPE; 
+BEGIN
+    SELECT START_DATE INTO v_date FROM v_loans WHERE loan_id = p_loan_id;
+    WHILE v_date < CURRENT_DATE LOOP
+        v_date := add_months(v_date, 1);
+    END LOOP;
+    RETURN v_date;
+END;
+
+create or replace FUNCTION       "F_YEAR_INSTALLMENTS" (p_loan_id NUMBER)
+RETURN NUMBER
+AS
+    v_flag          BOOLEAN := TRUE;
+    v_install_sum   v_loans.installment%TYPE := 0;
+    v_install       v_loans.installment%TYPE;
+    v_date          v_loans.START_DATE%TYPE := f_next_installment(p_loan_id);
+    v_balance       v_loans.BALANCE%TYPE;
+    v_new_year      v_loans.START_DATE%TYPE  := ROUND(add_months(CURRENT_DATE, 6), 'YEAR');
+BEGIN
+    SELECT installment INTO v_install FROM v_loans WHERE loan_id = p_loan_id;
+    SELECT balance INTO v_balance FROM v_loans WHERE loan_id = p_loan_id;
+    WHILE v_date < v_new_year AND v_flag LOOP
+        IF v_install_sum + v_install > v_balance THEN
+            v_flag := FALSE;
+            v_install_sum := v_balance;
+        ELSE 
+            v_install_sum := v_install + v_install_sum;
+        END IF;
+        v_date := add_months(v_date,1);
+    END LOOP;
+
+    RETURN v_install_sum;
 END;
