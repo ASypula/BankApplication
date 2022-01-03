eb77a5c53fc474678e7c2f80552cfe9f917deb52
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2022-01-03 17:39:27 +0100

    Bank Branch class with opening hours as a dictionary

---
+++ Bank_app/Branch.java
@@ -0,0 +1,56 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Branch {
+	static String[] days = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+	Map<String, String> Opening_hours = new HashMap<String, String>();
+	String bank_branch_id, addresses_address_id;
+	int employees_no;
+	public Map<String, String> getOpening_hours() {
+		return Opening_hours;
+	}
+	public String getOpening_hours(String day) {
+		return Opening_hours.get(day);
+	}
+	public String getBank_branch_id() {
+		return bank_branch_id;
+	}
+	public String getAddresses_address_id() {
+		return addresses_address_id;
+	}
+	public int getEmployees_no() {
+		return employees_no;
+	}
+	public Address getAddress() throws SQLException, WrongId {
+		return new Address(addresses_address_id);
+	}
+	public Branch(String bank_branch_id)  throws SQLException, WrongId {
+		Statement statement = Main.conn.createStatement();
+		ResultSet results = statement.executeQuery("SELECT OPENING_HOURS_ID, addresses_address_id, employees_no from BANK_BRANCHES where bank_branch_id = "+bank_branch_id);
+		if (results.next()) { // if not empty
+			String opening_hours_id = results.getString(1);
+			this.bank_branch_id = bank_branch_id;
+			this.addresses_address_id = results.getString(2);
+			this.employees_no = results.getInt(3);
+			results = statement.executeQuery("SELECT MON_HOURS, TUE_HOURS, WED_HOURS,"
+			+ "THU_HOURS, FRI_HOURS,SAT_HOURS,SUN_HOURS from OPENING_HOURS where OPENING_HOURS_ID = "+opening_hours_id);
+			if (results.next()) { // if not empty
+				int i = 1;
+				for (String day : days) {
+					Opening_hours.put(day, results.getString(i++));
+				}
+			} else
+				throw new WrongId(opening_hours_id);
+		} else
+			throw new WrongId(bank_branch_id);
+	}
+	@Override
+	public String toString() {
+		return "Branch [Opening_hours=" + Opening_hours + ", bank_branch_id=" + bank_branch_id
+				+ ", addresses_address_id=" + addresses_address_id + ", employees_no=" + employees_no + "]";
+	}
+
+}
